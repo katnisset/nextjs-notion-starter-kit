fb18d2709053c6e2bf4ead03ab1d69b4a46e4592
katnisset <1312477099@qq.com>
2024-07-08 17:32:37 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,15 +2,15 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '6e2f0106b5404f61b04f111118111bb1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  name: 'Katnisset',
+  domain: 'https://candied-jet-8cb.notion.site/Katnisset-6e2f0106b5404f61b04f111118111bb1',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
